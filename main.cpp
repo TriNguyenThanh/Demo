db311c9161a03fb4831aec29b6d8aa5ae9cd3565
BienBeo <tranngocbien2809@gmail.com>
2024-09-09 15:39:26 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "base.h"
 #include "tich.h"
+#include "hieu.h"
 int main(){
-<<<<<<< HEAD
     std::cout << "\nTich la: " << tich(2,2);
-=======
-    std:: cout << tich(2,2);
->>>>>>> MinhTu
+    std::cout << tich(2,2);
+    cout << hieu(5, 2);
     return 0;
 }
