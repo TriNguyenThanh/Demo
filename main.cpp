896c73af203843fc1453ad8b33a076e3ba36937c
TrisLord <tringuyenntt1505@gmail.com>
2024-09-09 14:10:30 +0700

    reset

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "tong.h"
-#include "hieu.h"
 #include "thuong.h"
 
 int main(){
     output();
     std::cout << "\nTong la: " << tong(2, 2);
-    std::cout << "\nHieu la: " << hieu(2, 2);
     std::cout << "\nThuong la: " << thuong(2, 2);
     return 0;
 }
