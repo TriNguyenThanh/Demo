e26d4b4ee74eac0a8a59a992842d81d1dbc8b65d
TrisLord <tringuyenntt1505@gmail.com>
2024-09-09 14:03:07 +0700

    thuong.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "tong.h"
+#include "thuong.h"
 
 int main(){
     output();
     std::cout << "\nTong la: " << tong(2, 2);
+    std::cout << "\nThuong la: " << thuong(2, 2);
     return 0;
 }
---
+++ thuong/thuong.cpp
@@ -0,0 +1,5 @@
+#include "thuong.h"
+
+int thuong(int a, int b){
+    return a/b;
+}
